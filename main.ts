967b224ed9d8cb8bda64fedb41e25c96056a560b
joh-na-than <151866818+joh-na-than@users.noreply.github.com>
2023-11-23 12:40:06 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+basic.clearScreen()
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . . # . .
+    # # # # #
+    . # # # .
+    `)
+basic.clearScreen()
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . # . .
+    # # # # #
+    `)
+basic.clearScreen()
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . # . .
+    `)
+basic.clearScreen()
